baf54fb8ab7b17effbea2f84b53dd4533704340e
aucguy <null>
2020-03-27 23:33:08 -0500

    Refactor executeTest.c
    This will make adding future tests easier.

--- src/test/executeTest.c
+++ src/test/executeTest.c
@@ -25,6 +25,76 @@ Module* sourceToModule(const char* src) {
     return module;
 }
 
+typedef struct {
+    Runtime* runtime;
+    const char* src;
+    const char* name;
+    Thing** args;
+    uint8_t arity;
+} ExecFuncIn;
+
+typedef struct {
+    Thing* retVal;
+    const char* errorMsg;
+    Module* module;
+} ExecFuncOut;
+
+ExecFuncOut execFunc(ExecFuncIn in) {
+    ExecFuncOut out;
+    out.retVal = NULL;
+    out.errorMsg = NULL;
+    out.module = NULL;
+
+    uint8_t error = 0;
+    out.module = sourceToModule(in.src);
+    if(out.module == NULL) {
+        out.errorMsg = "error in source code";
+        return out;
+    }
+
+    Thing* global = executeModule(in.runtime, out.module, &error);
+    if(error != 0) {
+        out.errorMsg = "error executing global scope";
+        return out;
+    } else if(typeOfThing(global) != THING_TYPE_OBJ) {
+        out.errorMsg = "global scope is not an object";
+        return out;
+    }
+
+    Thing* func = getObjectProperty(global, in.name);
+    if(func == NULL) {
+        out.errorMsg = "function not found";
+        return out;
+    } else if(typeOfThing(func) != THING_TYPE_FUNC) {
+        out.errorMsg = "function is not a function";
+        return out;
+    }
+
+    out.retVal = callFunction(in.runtime, func, in.arity, in.args, &error);
+    if(error != 0) {
+        out.errorMsg = "error executing add function";
+        return out;
+    }
+    return out;
+}
+
+void cleanupExecFunc(ExecFuncIn in, ExecFuncOut out) {
+    destroyRuntime(in.runtime);
+    destroyModule(out.module);
+    free(in.args);
+    deinitThing();
+}
+
+uint8_t checkInt(Thing* thing, int32_t num) {
+    if(typeOfThing(thing) != THING_TYPE_INT) {
+        return 0;
+    } else if(thingAsInt(thing) != num) {
+        return 0;
+    } else {
+        return 1;
+    }
+}
+
 const char* executeTestGlobalHasMainFunc() {
     initThing();
     Runtime* runtime = createRuntime();
@@ -48,54 +118,40 @@ const char* executeTestGlobalHasMainFunc() {
 
 const char* executeTestMainFuncReturns1() {
     initThing();
-    Runtime* runtime = createRuntime();
-    uint8_t error = 0;
-    Module* module = sourceToModule("def main x do <- 1; end");
-    assert(module != NULL, "error in source code");
 
-    Thing* global = executeModule(runtime, module, &error);
-    Thing* mainFunc = getObjectProperty(global, "main");
-    Thing** args = malloc(sizeof(Thing*) * 1);
-    args[0] = runtime->noneThing;
-    Thing* retVal = callFunction(runtime, mainFunc, 1, args, &error);
-    assert(error == 0 , "failed to execute the function");
+    ExecFuncIn in;
+    in.runtime = createRuntime();
+    in.src = "def main x do <- 1; end";
+    in.name = "main";
+    in.arity = 1;
+    in.args = malloc(sizeof(Thing*) * in.arity);
+    in.args[0] = in.runtime->noneThing;
 
-    assert(typeOfThing(retVal) == THING_TYPE_INT, "return value is not an integer");
-    assert(thingAsInt(retVal) == 1, "return value is not 1");
+    ExecFuncOut out = execFunc(in);
+    assert(out.errorMsg == NULL, out.errorMsg);
+    assert(checkInt(out.retVal, 1), "return value is not 1");
 
-    free(args);
-    destroyModule(module);
-    destroyRuntime(runtime);
-    deinitThing();
+    cleanupExecFunc(in, out);
     return NULL;
 }
 
 const char* executeTestAddSubFunction() {
     initThing();
-    Runtime* runtime = createRuntime();
-    uint8_t error = 0;
-    Module* module = sourceToModule("def add_sub x y z do <- x + y - z; end");
-    assert(module != NULL, "error in source code");
 
-    Thing* global = executeModule(runtime, module, &error);
-    assert(error == 0, "error executing global scope");
-    assert(typeOfThing(global) == THING_TYPE_OBJ, "global scope is not an object");
-    Thing* addFunc = getObjectProperty(global, "add_sub");
-    assert(addFunc != NULL, "add_sub function not found")
-    assert(typeOfThing(addFunc) == THING_TYPE_FUNC, "add_sub function is not a function");
+    ExecFuncIn in;
+    in.runtime = createRuntime();
+    in.src = "def add_sub x y z do <- x + y - z; end";
+    in.name = "add_sub";
+    in.arity = 3;
+    in.args = malloc(sizeof(Thing*) * in.arity);
+    in.args[0] = createIntThing(in.runtime, 1);
+    in.args[1] = createIntThing(in.runtime, 2);
+    in.args[2] = createIntThing(in.runtime, 3);
 
-    Thing** args = malloc(sizeof(Thing*) * 3);
-    args[0] = createIntThing(runtime, 1);
-    args[1] = createIntThing(runtime, 2);
-    args[2] = createIntThing(runtime, 3);
-    Thing* retVal = callFunction(runtime, addFunc, 3, args, &error);
-    assert(error == 0, "error executing add function");
-    assert(typeOfThing(retVal) == THING_TYPE_INT, "returned value is not an integer");
-    assert(thingAsInt(retVal) == 0, "returned value is not 0");
+    ExecFuncOut out = execFunc(in);
+    assert(out.errorMsg == NULL, out.errorMsg);
+    assert(checkInt(out.retVal, 0), "returned value is not 0");
 
-    destroyRuntime(runtime);
-    destroyModule(module);
-    free(args);
-    deinitThing();
+    cleanupExecFunc(in, out);
     return NULL;
 }
